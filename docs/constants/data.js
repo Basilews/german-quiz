69afae68965392475f5b668e5bad5c4a8e3e3158
Basilews <dethroll@gmail.com>
2018-12-02 23:14:55 +0300

    add gitignore & docs folder

---
+++ docs/constants/data.js
@@ -0,0 +1,44 @@
+export default {
+  "words": [
+    {
+      "word": "Untendemschwankendentischbuch",
+      "description": {
+        "en": "The book you're propping up the wobbly desk with",
+        "ru": "Книга, которой ты подпираешь шатающийся стол",
+      },
+      "isGerman": false,
+    },
+    {
+      "word": "Vergangenkriegealliierteverräter",
+      "description": {
+        "en": "Traitors who were allies in past wars",
+        "ru": "Предатели, которые были союзниками в прошлых войнах",
+      },
+      "isGerman": false,
+    },
+    {
+      "word": "Lebensversicherungsgesellschaft",
+      "description": {
+        "en": "A life insurance company",
+        "ru": "Компания по страхованию жизни"
+      },
+      "isGerman": true
+    },
+    {
+      "word": "Nahrungsmittelunverträglichkeitsreaktion",
+      "description": {
+        "en": "Adverse food reaction",
+        "ru": "Побочная пищевая реакция",
+      },
+      "isGerman": true,
+    },
+    {
+      "word": "Fussbodenschleifmaschinenverleih",
+      "description": {
+        "en": "Floor grinding machine rent",
+        "ru": "Прокат машин для шлифования пола",
+      },
+      "isGerman": true,
+    }
+  ]
+}
